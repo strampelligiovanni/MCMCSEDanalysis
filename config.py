90905c547eed4add19b3d8900f9d50b8c8e46c39
strampelligiovanni <strampelligiovanni@gmail.com>
2024-12-03 11:58:00 +0000

    updated config file

--- config.py
+++ config.py
@@ -5,10 +5,5 @@ Created on Fri Jan 17 12:10:40 2020
 
 @author: gstrampelli
 """
-import platform
-OS=platform.system()
-User=platform.node()
-Platform=platform.platform()
-
-path2projects='E:/Projects'
-path2data='E:/Data'
+path2projects='/Users/gstrampelli/PycharmProjects/Projects'
+path2data='/Users/gstrampelli/PycharmProjects/Projects/Prof_Tan'
