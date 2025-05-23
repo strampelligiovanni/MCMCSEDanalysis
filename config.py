ee3631f3c95d994cc2991aab4a9f3eefc3c509a2
gstrampelli <strampelligiovanni@gmail.com>
2025-05-23 10:31:41 +0100

    changed path to curent

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ Created on Fri Jan 17 12:10:40 2020
 
 @author: gstrampelli
 """
-path2projects='/Users/gstrampelli/PycharmProjects'
-path2data='/Users/gstrampelli/PycharmProjects/MCMC_analysis/Tutorial'
+path2projects='/Users/gstrampelli'
+path2data='/Users/gstrampelli/MCMC_analysis/Tutorial'
