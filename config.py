f64d4fc718e7eb8841184eadf0de0db3ca351689
gstrampelli <strampelligiovanni@gmail.com>
2025-05-23 11:07:21 +0100

    removed. not needed anymore now that we have the yaml file

--- config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 17 12:10:40 2020
-
-@author: gstrampelli
-"""
-path2projects='/Users/gstrampelli'
-path2data='/Users/gstrampelli/MCMC_analysis/Tutorial'
